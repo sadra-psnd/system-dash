70f27938b8b437fd90396845ec45fa395fb863c2
Sadra Pasandideh <mohammadsadrapasandideh@gmail.com>
2025-10-07 22:07:32 +0330

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import psutil
 
 app = Flask(__name__)
@@ -10,6 +10,19 @@ def dashboard():
     ram = psutil.virtual_memory().percent 
     disk = psutil.disk_usage('/').percent
     return render_template('dashboard.html', cpu=cpu, ram=ram, disk=disk)
+
+@app.route('/api/stats')
+
+def api_stats(): 
+    
+    data = {
+             'cpu': psutil.cpu_percent(),
+             'ram': psutil.virtual_memory().percent,
+             'disk': psutil.disk_usage('/').percent 
+    }
+
+    return jsonify(data)
+    
 
 if __name__ == "__main__" :     
     app.run(debug=True)
